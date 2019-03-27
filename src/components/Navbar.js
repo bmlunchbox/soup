7cacc2d9b08a05f3b1cd7d02f3a7596d469f3a5d
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-27 16:48:14 -0400

    new page referneces init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,10 @@ class Navbar extends Component {
         <h2><a>Kitchen Soup</a></h2>
         <nav>
           <li><a href="/">Home</a></li>
-          <li><a>Menu</a></li>
+          <li><a href="/menu">Create Menu</a></li>
           <li><a href="/recipe">Recipes</a></li>
-          <li><a>Inventory</a></li>
-          <li><a>About</a></li>
+          <li><a href="/inventory">Inventory</a></li>
+          <li><a href="/about">About</a></li>
         </nav>
       </header>
     );
