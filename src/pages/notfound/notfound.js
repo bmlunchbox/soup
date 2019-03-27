e625dd763c8090398a70dc8c5d5fd662d072cbdf
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-27 01:40:27 -0400

    404 page

---
+++ src/pages/notfound/notfound.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './notfound.css'
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div className="body">
+            	<div className="text-container">
+                	<h1>uhhhh nothing here</h1>
+                	<h2>Try something else.</h2>
+                </div>
+            </div>
+        );
+    }
+}
