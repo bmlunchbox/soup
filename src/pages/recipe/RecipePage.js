16e37349132af7448b6f75e9f3c92388efb7dedf
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-27 18:23:54 -0400

    popup modal

--- src/pages/recipe/RecipePage.js
+++ src/pages/recipe/RecipePage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import RecipeInput from './RecipeInput';
 import RecipeList from './RecipeList';
 import './RecipePage.css';
+
+Modal.setAppElement('#root');
 
 class RecipePage extends Component {
   constructor(props) {
@@ -13,21 +16,26 @@ class RecipePage extends Component {
           title: "Spaghetti",
           instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
           ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-          img: "https://images.unsplash.com/photo-1548247661-3d7905940716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
+          img: "https://images.unsplash.com/photo-1548247661-3d7905940716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
+          dietary: ["nuts", "halal"],
+          portions: 10
         },
         {
           id: 1,
           title: "Milkshake",
           instructions: "Combine ice cream and milk.  Blend until creamy",
           ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-          img: "https://images.unsplash.com/photo-1541658016709-82535e94bc69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+          img: "https://images.unsplash.com/photo-1541658016709-82535e94bc69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+          dietary: ["nuts"],
+          portions: 10
         },
         { 
           id: 2,
           title: "Avocado Toast",
           instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
           ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
-          img: "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
+          img: "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
+          portions: 5
         }
       ],
       nextRecipeId: 3,
@@ -36,6 +44,16 @@ class RecipePage extends Component {
     
     this.handleSave = this.handleSave.bind(this);
     this.onDelete = this.onDelete.bind(this);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal(){
+    this.setState({ showForm: true });
+  }
+
+  handleCloseModal(){
+    this.setState({ showForm: false });
   }
   
   handleSave(recipe) {
@@ -53,19 +71,18 @@ class RecipePage extends Component {
     const recipes = this.state.recipes.filter(r => r.id !== id);
     this.setState({recipes});
   }
-
+  
   render() {
     const {showForm} = this.state;
     return (
       <div>
-        <button className="button-save" onClick={() => this.setState({showForm: true})}>Add New</button>
-        { showForm ?
-            <RecipeInput 
-              onSave={this.handleSave}
-              onClose={() => this.setState({showForm: false})}  
-            /> :
-            null 
-        }
+        <button className="button-save" onClick={this.handleOpenModal}>Add New</button>
+        <Modal isOpen={this.state.showForm}>
+          <RecipeInput 
+            onSave={this.handleSave} 
+            onClose={this.handleCloseModal}
+          />
+        </Modal>        
         <RecipeList onDelete={this.onDelete} recipes={this.state.recipes} />
       </div>
     );
