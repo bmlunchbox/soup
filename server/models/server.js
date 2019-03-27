07033c8982a4ccba2eb28e419d40645c670ad3b6
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-27 02:15:52 -0400

    server fix, recipe routes working

--- server/models/server.js
+++ server/models/server.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+
 mongoose.set('debug', true);
-mongoose.connect("mongodb+srv://brandon:Brandon@soup-dmnpg.mongodb.net/test?retryWrites=true", {useNewUrlParser: true},
+mongoose.connect("mongodb+srv://brandon:<pw>@soup-dmnpg.mongodb.net/Soup?retryWrites=true", {useNewUrlParser: true},
 	function(err){
 		if(err){
 			console.log("DB failed.");
