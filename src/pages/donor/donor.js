373ab1b2e512e5a05583e089a71530eafa49394d
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-27 19:41:22 -0400

    donor

---
+++ src/pages/donor/donor.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './donor.css';
+
+class Donor extends Component {
+	constructor(props){
+		super(props);
+		this.state = {};
+	}
+
+	render(){
+		return(
+			<h1>Donor Page</h1>
+		);
+	}
+}
+
+export default Donor;
