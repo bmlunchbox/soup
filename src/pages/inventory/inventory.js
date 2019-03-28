cd05b89aa8a91c194dc710f16f6b1b194c9b8c6a
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-28 16:47:39 -0400

    inventory page html structure

--- src/pages/inventory/inventory.js
+++ src/pages/inventory/inventory.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
+import { Table } from 'semantic-ui-react';
 import './inventory.css';
+
+const RowEntry = (elem) => {
+
+}
 
 class Inventory extends Component {
 	constructor(props){
@@ -9,7 +14,41 @@ class Inventory extends Component {
 
 	render(){
 		return(
-			<h1>Inventory Page</h1>
+			<div className="container">
+				<h2 className="title">Inventory</h2>
+				<div>
+					<button className="button-inventory">Add Item</button>
+				</div>
+				<div className="table-container">
+					<Table celled className="ui table">
+						<Table.Header>
+							<Table.Row>
+								<Table.HeaderCell textAlign='center' className="item-col">Item</Table.HeaderCell>
+								<Table.HeaderCell textAlign='center'>Stock</Table.HeaderCell>
+							</Table.Row>
+						</Table.Header>
+
+						<Table.Body>
+							<Table.Row>
+								<Table.Cell>Item Name</Table.Cell>
+								<Table.Cell>
+									<div className="ui transparent input">
+										<input type="text" placeholder="0"/>
+									</div>
+								</Table.Cell>
+							</Table.Row>
+							<Table.Row>
+								<Table.Cell>Item Name</Table.Cell>
+								<Table.Cell>
+									<div className="ui transparent input">
+										<input type="text" placeholder="0"/>
+									</div>
+								</Table.Cell>
+							</Table.Row>
+						</Table.Body>
+					</Table>
+				</div>	
+			</div>
 		);
 	}
 }
