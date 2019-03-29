e07c0b556406deb2ed4661910e43ff5ffa22f3a8
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-29 15:27:40 -0400

    menu page structure

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,15 +1,65 @@
 import React, {Component} from 'react';
+import { Input, Table, Button } from 'semantic-ui-react'; 
 import './menu.css';
 
 class Menu extends Component {
 	constructor(props){
 		super(props);
-		this.state = {};
+		this.state = {
+			menu: [],
+			recipe: [],
+			restrictions: []
+		};
 	}
 
 	render(){
 		return(
-			<h1>Menu Page</h1>
+			<div>
+				<h1 className="main-title">Menu</h1>
+				<div className="portion-container">
+					<label className="sub-title">Target Portions: </label>
+					<span className="portion-target-input">
+						<Input className="portion-target-input" type="number" defaultValue={100}></Input>
+					</span>
+				</div>
+				<div className="section-one">
+					<div className="menu-table">
+						<Table className="ui table">
+							<Table.Header>
+								<Table.Row>
+									<Table.HeaderCell className="eight wide" textAlign='center'>Menu Item</Table.HeaderCell>
+									<Table.HeaderCell className="four wide" textAlign='center'>Portions</Table.HeaderCell>
+									<Table.HeaderCell className="four wide" textAlign='center'>Restriction</Table.HeaderCell>
+								</Table.Row>
+							</Table.Header>
+						</Table>
+					</div>
+					<div className="restriction-table">
+						<Table className="ui table celled">
+							<Table.Header>
+								<Table.Row>
+									<Table.HeaderCell className="ten wide" textAlign='center'>Restriction Type</Table.HeaderCell>
+									<Table.HeaderCell className="six wide" textAlign='center'>Estimated</Table.HeaderCell>
+								</Table.Row>
+							</Table.Header>
+						</Table>
+					</div>
+					<Button>Reset</Button>
+				</div>
+				<h1>Recipes</h1>
+				<div className="recipe-table">
+					<Table className="ui table celled">
+						<Table.Header>
+							<Table.Row>
+								<Table.HeaderCell className="six wide" textAlign='center'>Recipe</Table.HeaderCell>
+								<Table.HeaderCell className="three wide" textAlign='center'>Available</Table.HeaderCell>
+								<Table.HeaderCell className="four wide" textAlign='center'>Restriction</Table.HeaderCell>
+								<Table.HeaderCell className="three wide" textAlign='center'>Portions</Table.HeaderCell>
+							</Table.Row>
+						</Table.Header>
+					</Table>
+				</div>
+			</div>
 		);
 	}
 }
